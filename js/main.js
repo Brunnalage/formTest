98fbedeb65fe2b51687b45f6b3f6e5d9a4db07f9
Brunnalage <brunnalage@gmail.com>
2022-06-03 13:47:45 -0300

    Feat: Add page redirect functionality on register button click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+/*Button register*/
+document.getElementById("btn-register").onclick = function () {
+    location.href = "#login-page";
+}
+
+/*Button go back*/
 document.getElementById("btn-back").onclick = function () {
     location.href = "#register-page";
 }
