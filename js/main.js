f8a5454631205a933280700bd2731cb1cddf78fe
Brunna Lage <91227939+Brunnalage@users.noreply.github.com>
2022-06-06 02:18:47 -0300

    "Docs:  Removing unused files"

--- js/main.js
+++
@@ -1,9 +0,0 @@
-/*Button register*/
-document.getElementById("btn-register").onclick = function () {
-    location.href = "#login-page";
-}
-
-/*Button go back*/
-document.getElementById("btn-back").onclick = function () {
-    location.href = "#register-page";
-}
